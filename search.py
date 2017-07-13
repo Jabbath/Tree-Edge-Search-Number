f65f3009f0f57120673448cd2f99c55055583f10
Anton Afanassiev <antonafana@yahoo.ca>
2017-07-13 15:30:52 -0230

    Finished testing. Functional.

--- search.py
+++ search.py
@@ -233,5 +233,5 @@ def computeInfo(tree):
 
     return information
 
-G = nx.read_multiline_adjlist('tests/doubling/1.adjlist')
+G = nx.read_multiline_adjlist('tests/doubling/63.adjlist')
 print computeInfo(G)
